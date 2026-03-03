4838610441dede515c6fdfe1bbe0399994dbc1ca
Ashley Kier Ferreol <ashleykierferreol@gmail.com>
2026-03-03 15:53:48 +0800

    fix deploy errors

--- src/components/FloatingChatbot.tsx
+++ src/components/FloatingChatbot.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MessageCircle, Bot } from "lucide-react";
+import { Bot } from "lucide-react";
 import ITChatbot from "./ITChatbot";
 
 interface FloatingChatbotProps {
